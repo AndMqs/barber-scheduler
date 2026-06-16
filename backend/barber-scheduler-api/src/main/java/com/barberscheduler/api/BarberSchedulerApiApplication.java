ab8f28db3d471d2b30b0b15d98a12233ca80f122
AndMqs <andresa.b.marques@hotmail.com>
2026-06-16 15:43:36 -0300

    feat: create initial Spring Boot project

---
+++ backend/barber-scheduler-api/src/main/java/com/barberscheduler/api/BarberSchedulerApiApplication.java
@@ -0,0 +1,14 @@
+package com.barberscheduler.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BarberSchedulerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberSchedulerApiApplication.class, args);
+    }
+
+}
---
+++ backend/barber-scheduler-api/src/test/java/com/barberscheduler/api/BarberSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barberscheduler.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberSchedulerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
